9353f530d9b1e3d20ef1c5bca2fc950940614dee
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 13:40:10 +0100

    Update PruebahashMap.java

--- PruebahashMap.java
+++ PruebahashMap.java
@@ -15,54 +15,11 @@ public class PruebahashMap {
         map.put("iris", "flower");
         map.put("potato", "tuber");
         
+        //muestra las llaves y los valores
         for(Map.Entry<String, String> pair : map.entrySet()){
             String key = pair.getKey();
             String value = pair.getValue();
             System.out.println(key + " - " + value);
-        }   
+        }
     }
-//Mostrar las llaves:
-public static void main(String[] args) throws Exception {
-  HashMap<String, String> map = new HashMap<String, String>();
-  map.put("Sim", "Sim");
-  map.put("Tom", "Tom");
-  map.put("Arbus", "Arbus");
-  map.put("Baby", "Baby");
-  map.put("Cat", "Cat");
-  map.put("Dog", "Dog");
-  map.put("Eat", "Eat");
-  map.put("Food", "Food");
-  map.put("Gevey", "Gevey");
-  map.put("Hugs", "Hugs");
-  
-  printKeys(map);
-}
- public static void printKeys(Map<String, String> map) {
-   for(String key : map.keySet()){
-     System.out.println(key);
-     }
-}
-
-//Mostrar los valores: 
-public static void main(String[] args) throws Exception {
-  HashMap<String, String> map = new HashMap<String, String>();
-  map.put("Sim", "Sim");
-  map.put("Tom", "Tom");
-  map.put("Arbus", "Arbus");
-  map.put("Baby", "Baby");
-  map.put("Cat", "Cat");
-  map.put("Dog", "Dog");
-  map.put("Eat", "Eat");
-  map.put("Food", "Food");
-  map.put("Gevey", "Gevey");
-  map.put("Hugs", "Hugs");
-  
-  printValues(map);
-  }
-
-  public static void printValues(Map<String, String> map) {
-    for(String value : map.values()){
-      System.out.println(value);
-      }
-   }    
 }
