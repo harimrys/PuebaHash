47ef613db0f2a1c50c9fe6c9335758e0dfee97cd
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 13:46:07 +0100

    Update and rename PruebahashMap3.java to PruebahashMapVALUE.java

--- PruebahashMapVALUE.java
+++ PruebahashMapVALUE.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class PruebahashMap {
+public class PruebahashMapVALUE {
   //Mostrar los valores:
   public static void main(String[] args) throws Exception {
     HashMap<String, String> map = new HashMap<String, String>();
