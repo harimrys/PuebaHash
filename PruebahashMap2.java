dd396e15a7d3fd268cb41d5ca428198e3204c8e6
Hari <93679710+harimrys@users.noreply.github.com>
2023-05-17 13:41:12 +0100

    Create PruebahashMap2.java

---
+++ PruebahashMap2.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class PruebahashMap2 {
+
+//Mostrar las llaves:
+public static void main(String[] args) throws Exception {
+  HashMap<String, String> map = new HashMap<String, String>();
+  map.put("Sim", "Sim");
+  map.put("Tom", "Tom");
+  map.put("Arbus", "Arbus");
+  map.put("Baby", "Baby");
+  map.put("Cat", "Cat");
+  map.put("Dog", "Dog");
+  map.put("Eat", "Eat");
+  map.put("Food", "Food");
+  map.put("Gevey", "Gevey");
+  map.put("Hugs", "Hugs");
+  
+  printKeys(map);
+}
+ public static void printKeys(Map<String, String> map) {
+   for(String key : map.keySet()){
+     System.out.println(key);
+     }
+ }
+}
